3dada05462241e51e6d58347edaf38f50fc0080a
Szymon Rogowski <szymon0r0official@gmail.com>
2022-12-20 05:11:32 +0100

    db connect

---
+++ REST_API_MONGO/db_connection.go
@@ -0,0 +1,34 @@
+package REST_API_MONGO
+
+import (
+	"context"
+
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"log"
+	"time"
+)
+
+func ConnectDB() *mongo.Collection {
+	Mongo_URI := ""
+	client, err := mongo.NewClient(options.Client().ApplyURI(Mongo_URI))
+	// client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(Mongo_URI))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	defer cancel()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Ping the primary
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected to mongoDB and pinged.")
+	collection := client.Database("").Collection("")
+	return collection
+}
