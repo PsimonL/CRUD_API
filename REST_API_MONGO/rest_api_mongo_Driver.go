85a76302c64fc43e10efb2abe2c42d4c0bf49396
Szymon Rogowski <szymon0r0official@gmail.com>
2022-12-20 05:10:44 +0100

    Rest Api mongo

---
+++ REST_API_MONGO/rest_api_mongo_Driver.go
@@ -0,0 +1,25 @@
+package REST_API_MONGO
+
+import (	"fmt"
+			"github.com/gin-gonic/gin"
+			//"log"
+			//"gopkg.in/mgo.v2/bson"
+	)
+
+func rest_api_mongo_Driver(){
+	fmt.Println("Working Mongo driver code")
+	router := gin.Default()
+
+	//var collection = ConnectDB()
+	//router.POST("/post", func(c *gin.Context) {
+	//	doc := bson.D{{"_id", 5555}, {"ad", "ag"}, {"ba", "bd"}, {"ce", 5555}}
+	//	result, err := collection.InsertOne(context.TODO(), doc)
+	//	print(err)
+	//	print(result)
+	//	c.IndentedJSON(http.StatusCreated, doc)
+	//	fmt.Println("Isnerted: ", doc)
+	//})
+	router.POST("/post", CreateSingleDataSetPOST)
+	router.Run("localhost:8080")
+}
+}
